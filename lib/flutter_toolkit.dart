4013b6056a09e1696d2aab62b004fedf899d3705
XDD666 <xdd666@foxmail.com>
2021-03-17 16:19:42 +0800

    增加日志库和通用回调

--- lib/flutter_toolkit.dart
+++ lib/flutter_toolkit.dart
@@ -1,3 +1,10 @@
 library flutter_toolkit;
 
+///网络库
 export 'package:flutter_toolkit_easy/src/net/net_util.dart';
+
+///日志库
+export 'package:flutter_toolkit_easy/src/utils/info/log_util.dart';
+
+///通用callback回调
+export 'package:flutter_toolkit_easy/src/dialog/assist/typedef/function.dart';
