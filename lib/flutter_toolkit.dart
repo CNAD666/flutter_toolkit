e340b31e9becb4d5e0c786ea1b390683c7af7136
XDD666 <35656085+CNAD666@users.noreply.github.com>
2021-04-18 16:46:17 +0800

    增加保活类

--- lib/flutter_toolkit.dart
+++ lib/flutter_toolkit.dart
@@ -9,5 +9,6 @@ export 'package:flutter_toolkit_easy/src/utils/info/log_util.dart';
 ///通用callback回调
 export 'package:flutter_toolkit_easy/src/dialog/assist/typedef/function.dart';
 
-///UI适配
+///UI适配  保活页面
 export 'package:flutter_toolkit_easy/src/utils/ui/ui_util.dart';
+export 'package:flutter_toolkit_easy/src/utils/ui/keep_alive_page.dart';
